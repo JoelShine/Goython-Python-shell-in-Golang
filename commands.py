e6dff500e72d2783e6b51694d5a2e6057d8523ae
Joel Shine <70741696+JoelShine@users.noreply.github.com>
2021-05-22 16:42:28 +0530

    Add files via upload

---
+++ commands.py
@@ -0,0 +1,6 @@
+'''
+Basic python commands now available in Goython -
+1 ) Print - print("Your command here")
+
+Now currently only supports print statements. Other versions are about to release !
+'''
